7ec0d800b76ee4cb9044e2ff16ea7221fd06a702
axelcall123 <axel15calderon@gmail.com>
2020-09-22 13:24:52 -0600

    ?

--- AutomataAon.py
+++ AutomataAon.py
@@ -2,8 +2,20 @@ import Principal
 Principal
 def automata(MatrizAon):#OBTENER 1.AON 2.AON LEIDO
     for id in range(len(MatrizAon)):
-        #print(str(MatrizAon[id].read())," wamos")
+        #print(type(MatrizAon[id].read())," wamos")
         SeparacionAutomata(MatrizAon[id].read())
 
 def SeparacionAutomata(Texto):
-    print()
+    Texto=Texto+"@$#$@"
+    #print(Texto)
+    palabrad=''
+    state=-1
+    SinTabAEs=Texto.replace(" ","").split()#RENPLAZANDO ESPACIOS
+    nueva_cadena="".join(SinTabAEs)#REMPLAZANDO TABS
+    nueva_cadena=nueva_cadena.lower()#TODO EN MINUSCULAS
+    print(nueva_cadena)
+
+    for i in range(len(nueva_cadena)):
+        print()
+        if state==-1:#BIENDO EL (
+            print()
