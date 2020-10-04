db4861c83d5de8babe015f28b3dbbbf1a7c3f611
axelcall123 <axel15calderon@gmail.com>
2020-10-04 11:43:56 -0600

    up

--- regex.py
+++ regex.py
@@ -1,31 +1,38 @@
 def AutomataFor(T,A):#AUTOMATA ||TEXTO EVALUADO AUTOMATA
     for id in range(len(T)):
-
         if T[id]==A[id]:#SI SON IGUALES LAS POSCIONES a=a
             if (len(T)-1)==id:#SI LLEGA EN LA ULTIMA POSICION
                 print(A,'^ PARENTESIS')
+        elif T[id]=='.'  and A[id]!='':
+            if (len(T)-1)==id:#SI LLEGA EN LA ULTIMA POSICION
+                print(A,'^ PARENTESIS')
+            continue
         else:#a=B
-            print('Nop')
+            #print('Nop')
             break
 
 def regresion(T,C,V1,V2):#TEXTO|CONDICION|VECES LIM IN|VECES LIM SUP
-    unir=''
+    #unir=''#.+|*|?
     con=0
     for id in range(len(T)):
-        if T[id]==C:#CARACTER IGUAL CONDICION a=a
+        if C==T[id]:#CARACTER IGUAL CONDICION a=a
            #unir=unir+T[id]
            con+=1#AUMENTA LAS VECES APARECIDOS
            #print('y')
         #elif T[id]!=' ':
             #unir=unir+T[id]
             #print('n')
+        elif C=='.' and T[id]!='':
+            con+=1
+            continue
+
     if V1<=con and V2>=con:# SI APARECE N VECES HASTA M VECES
         print(T,con," +*?")
     else:
         print('No Tiene Los Requisitos')
 
 def regresionFor(T,C,V1,V2):#TEXTO|CONDICION|VALOR1|VALOR2
-    union=''
+    union=''#(abc)+|*|?
     con=0
     pa=''#PALABRA O TEXTO DE LA CONDICION FILTRADA
     if C[len(C)-1]=='+' or  C[len(C)-1]=='?' or  C[len(C)-1]=='*':
@@ -46,10 +53,17 @@ def regresionFor(T,C,V1,V2):#TEXTO|CONDICION|VALOR1|VALOR2
  
     for a in range(len(T)):
         if T[a]==pa[0]:#SI TEXTO[a]==CONDICION[0]
-            if int(a+len(pa))<len(T):#posicion a + tamaño (ab) < tamaño(palabra)
+            if int(a+len(pa))<=len(T):#posicion a + tamaño (ab) < tamaño(palabra)
                 for i in range(1,len(pa)):
                     if T[a+i]==pa[i]:
-                      con+=1
+                        if i==int(len(pa)-1):#ULTIMA POSICION PARA NO CONTAR DEMAS
+                            con+=1
+                    elif pa[i]=='.' and T[a+i]!='':
+                        if i==int(len(pa)-1):
+                            con+=1
+                    else:
+                        #print('Nop')
+                        break#TERMINA
 
     if V1<=con and V2>=con:# SI APARECE N VECES HASTA M VECES
         print(T,con," +*?")
@@ -93,7 +107,7 @@ def reg(R,T):#EVALUACION REGEX|TEXTO EVALUADO
                 AutomataFor(paren,T)#TEXTO EVALUADO| AUTOMATA(si parece primero(ab))
             elif z[1]==T[0]:#a^ SI ES LA PRIMEAR LETRA a
                 print(T,'^ TEXTO')
-            elif z[1]!=T[0]:
+            elif z[1]=='.' and z[1]!='':#PUNTO CUALQUIERA
                 print(T,'^ TEXTO')
 
         elif z[len(z)-1]=='+':
@@ -101,18 +115,18 @@ def reg(R,T):#EVALUACION REGEX|TEXTO EVALUADO
                 regresionFor(T,z,1,10000)
             else:
                 regresion(T,z[len(z)-2],1,10000)
+
         elif z[len(z)-1]=='*':
             if z[len(z)-2]==')':
                 regresionFor(T,z,0,10000)
             else:
                 regresion(T,z[len(z)-2],0,10000)
+
         elif z[len(z)-1]=='?':
             if z[len(z)-2]==')':
                 regresionFor(T,z,0,1)
             else:
                 regresion(T,z[len(z)-2],0,1)
-
-reg('[^(A.E]','AXEL')
+#reg('[(JE)?]','JE')
 #regresion('holat','t',1)
 #regresionFor('abababa','(ab)+',0,0)
-
